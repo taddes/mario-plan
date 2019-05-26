dab7cffb9b59496d738fe3923de8b21e20975159
Taddes Korris <taddes.korris@passportinc.com>
2019-05-26 19:39:58 -0400

    basic config

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ProjectDetails(props) {
+  console.log(props)
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">TItle</span>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad quis dolore amet veritatis quam. Modi deleniti assumenda cupiditate aliquam quisquam harum necessitatibus doloremque corporis, ex error exercitationem quae ipsa reiciendis!</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by the Ninja</div>
+          <div>Date: 2 sept 2018</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
