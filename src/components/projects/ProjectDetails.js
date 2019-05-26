c9013a7bc1c59b1cea9ef07fd306578864939ea8
Taddes Korris <taddes.korris@passportinc.com>
2019-05-26 19:58:00 -0400

    basic auth setup

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 function ProjectDetails(props) {
-  console.log(props)
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">TItle</span>
+          <span className="card-title">TItle - {id}</span>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad quis dolore amet veritatis quam. Modi deleniti assumenda cupiditate aliquam quisquam harum necessitatibus doloremque corporis, ex error exercitationem quae ipsa reiciendis!</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
