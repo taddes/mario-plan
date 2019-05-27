cb72a9fa72860015aa5242dc1eed34a3ee48daf9
Taddes Korris <taddes.korris@passportinc.com>
2019-05-26 20:40:49 -0400

    configured reducers to map state to props

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: 1, title:'find Peach', content: 'blah blah test'},
+    {id: 2, title:'Race on shells', content: 'blah blah test'},
+    {id: 3, title:'Defeat bowser', content: 'blah blah test'}
+  ]
+}
 const projectReducer = (state = initState, action) => {
   return state
 }
