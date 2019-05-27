911d8b309f78b04620dfb55cea0e25072ac411ed
Taddes Korris <taddes.korris@passportinc.com>
2019-05-26 21:10:30 -0400

    firebase setup

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC5LwIFxxG8LBBaDzOJRj_srrd8E0i6Rus",
+  authDomain: "mario-plan-24561.firebaseapp.com",
+  databaseURL: "https://mario-plan-24561.firebaseio.com",
+  projectId: "mario-plan-24561",
+  storageBucket: "mario-plan-24561.appspot.com",
+  messagingSenderId: "807040737187",
+  appId: "1:807040737187:web:f0ef0776c2e8362b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase
