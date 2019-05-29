d9690054f7d6038609f06ad8c713d1a40911acb0
Taddes Korris <taddes.korris@passportinc.com>
2019-05-29 17:58:27 -0400

    authentication and firebase connection

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions'
 
 export class SignIn extends Component {
   state = {
@@ -12,9 +14,10 @@ export class SignIn extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state)
+    this.props.signIn(this.state)
   }
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -30,6 +33,9 @@ export class SignIn extends Component {
           <div className="input-field">
             <button className="btn pink lighten z-depth-0">Login</button>
           </div>
+          <div className="red-text center">
+            { authError ? <p>{authError}</p> : null }
+          </div>
         </form>
         
       </div>
@@ -37,4 +43,16 @@ export class SignIn extends Component {
   }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
